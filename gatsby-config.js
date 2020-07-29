d61bc75d6f639a83ff51e139ed58af2987242cfc
vivekkrvks <vivek.kr.vks@gmail.com>
2020-07-29 17:39:37 +0530

    Google font added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,54 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name:`pages`,
+        path:`${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name:`posts`,
+        path:`${__dirname}/src/posts`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:{
+        name:`images`,
+        path:`${__dirname}/src/images`
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-remark-images`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extentions:[`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: 'swap'
+      }
+    },
+  ],
 }
