7fe72afa80e57582c97775c60ec40bbc84467a7c
vivekkrvks <vivek.kr.vks@gmail.com>
2020-10-24 22:30:34 +0530

    some error changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {ThemeProvider} from "styled-components"
  
-import Theme from "./src/themes/themes"
+import Theme from "./src/themes/theme"
 
 export const wrapRootElement = ({ element }) => (
     <ThemeProvider theme={Theme}>{element}</ThemeProvider>
